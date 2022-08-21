9a65485893c0dd22f3a42033cf0d7135ddef217b
berkagmpp <berkagmpp@gmail.com>
2022-08-21 14:39:12 +1200

    feat: Add input event binding ngModel (two-way) in input element Add [(ngModel)] event property in servers.component.html Import and add FormsModule in app.module.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
     selector: '[app-servers]',
     templateUrl: './servers.component.html',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
     allowNewServer = false;
     serverCreationStatus = 'No server created!';
-    serverName='';
+    serverName='Test Server';
     
     constructor() {
         setTimeout(() => {
